7b0d9b6d6fd753bb799f244201cee4f7c4c02884
Gage Lodba <38990407+JerimiahOfficial@users.noreply.github.com>
2022-06-01 14:39:19 -0400

    Update BaseFoundationLimiter.cs

--- BaseFoundationLimiter.cs
+++ BaseFoundationLimiter.cs
@@ -14,20 +14,21 @@ Created by Jerimiah
 https://github.com/JerimiahOfficial/
 */
 
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Oxide.Plugins {
-    [Info("BaseFoundationLimiter", "Jerimiah", "0.0.1")]
-    [Description("A plugin to limits bases to a set amout of foundations.")]
+    [Info("BaseFoundationLimiter", "Jerimiah", "0.0.3")]
+    [Description("A plugin to limit players to a set amout of foundations.")]
 
     class FoundationLimiter : CovalencePlugin {
         private bool? CanBuild(Planner planner, Construction prefab, Construction.Target target) {
-            var buildingID = (target.entity as DecayEntity)?.buildingID;
+            uint? buildingID = (target.entity as DecayEntity)?.buildingID;
 
             if (buildingID == null) return null;
 
-            var building = BuildingManager.server.GetBuilding((uint)buildingID);
-            var foundations = building.buildingBlocks.Where(x => x.PrefabName.Contains("foundation"));
+            BuildingManager.Building building = BuildingManager.server.GetBuilding((uint)buildingID);
+            IEnumerable<BuildingBlock> foundations = building.buildingBlocks.Where(x => x.PrefabName.Contains("foundation"));
 
             if (foundations.Count() > 225) {
                 planner.GetOwnerPlayer().ChatMessage("You reached the maximum foundations.");
