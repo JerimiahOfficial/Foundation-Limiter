180e278f5fbdc2ec56025805e7fcc61a43b29761
Gage Lodba <38990407+JerimiahOfficial@users.noreply.github.com>
2022-06-01 16:26:53 -0400

    Update PlayerFoundationLimiter.cs

--- PlayerFoundationLimiter.cs
+++ PlayerFoundationLimiter.cs
@@ -9,9 +9,8 @@
 ⠀⠀⠀⠀⠀⠚⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠂
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⢻⣿⣿⡿⠛⠉⡇
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠘⠋⠁⠀⠀ ⣧
-
 Created by Jerimiah
-https://github.com/JerimiahOfficial/Foundation-Limiter
+https://github.com/JerimiahOfficial/
 */
 
 using System.Linq;
@@ -20,17 +19,13 @@ namespace Oxide.Plugins {
     [Info("PlayerFoundationLimiter", "Jerimiah", "0.0.3")]
     [Description("A plugin to limit players to a set amount of foundations.")]
 
-    class FoundationLimiter : CovalencePlugin {
+    class PlayerFoundationLimiter : CovalencePlugin {
         private bool? CanBuild(Planner planner, Construction prefab, Construction.Target target) {
-            if (prefab.fullName.Contains("foundation") && PlayerOverLimit(planner.GetOwnerPlayer())) {
+            if (prefab.fullName.Contains("foundation") && BaseNetworkable.serverEntities.OfType<BuildingBlock>().Count(x => x.name.Contains("foundation") && x.OwnerID == planner.GetOwnerPlayer().userID) > 100) {
                 planner.GetOwnerPlayer().ChatMessage("You've reached the maxium foundations.");
                 return false;
             }
             return null;
-        }
-
-        private bool PlayerOverLimit(BasePlayer player) {
-            return BaseNetworkable.serverEntities.OfType<BuildingBlock>().Count(x => x.name.Contains("foundation") && x.OwnerID == player.userID) > 100;
         }
     }
 }
